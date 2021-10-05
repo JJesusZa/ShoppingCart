f43279a0caa53ea46684839223245450a325874e
JJesusZa <jesuszamoraJDJZA@gmail.com>
2021-10-05 16:59:24 -0500

    listproducts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import React from 'react';
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: '',
+      sort: '',
+    };
+  }
+  render() {
+    return (
+      <div className='grid-container'>
+        <header>
+          <a href='/'>Moda por ZamsClu</a>
+        </header>
+        <main>
+          <div className='content'>
+            <div className='main'>
+              <Products products={this.state.products} />
+            </div>
+            <div className='sidebar'>Productos en el carrito</div>
+          </div>
+        </main>
+        <footer>Todos los derechos reservados</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
